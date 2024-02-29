b0ec75e55da8b8df77b88f80a3aeaa3c48c182d5
Suyashsmindfire <suyashs@mindfiresolutions.com>
2024-02-29 20:17:29 +0530

    First get route created [#4]

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { PORT } from './config';
+import { PORT } from '../Backend/config.js';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  return res.status(234).send('Inventory Management System');
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
